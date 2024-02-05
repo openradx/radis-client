d9ec7451dad8fc52b9bbcd30f3fb974552b58cf7
Kai Schlamp <kai.schlamp@gmail.com>
2024-02-05 18:02:42 +0000

    Load credentials from env file in notebook

--- tasks.py
+++ tasks.py
@@ -1,6 +1,11 @@
+from pathlib import Path
+from shutil import copy
+
 from invoke.context import Context
 from invoke.runners import Result
 from invoke.tasks import task
+
+project_dir = Path(__file__).resolve().parent
 
 
 def run_cmd(ctx: Context, cmd: str, silent=False) -> Result:
@@ -12,6 +17,12 @@ def run_cmd(ctx: Context, cmd: str, silent=False) -> Result:
     result = ctx.run(cmd, pty=True, hide=hide)
     assert result and result.ok
     return result
+
+
+@task
+def init_workspace(ctx: Context):
+    """Initialize workspace"""
+    copy(f"{project_dir}/example.env", f"{project_dir}/.env")
 
 
 @task
