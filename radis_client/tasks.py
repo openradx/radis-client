03f9f2399970e572a3097c7c5b50274b9e1ee98d
Kai Schlamp <kai.schlamp@gmail.com>
2024-02-05 12:51:51 +0000

    Added publish invoke task

---
+++ radis_client/tasks.py
@@ -0,0 +1,26 @@
+from invoke.context import Context
+from invoke.runners import Result
+from invoke.tasks import task
+
+
+def run_cmd(ctx: Context, cmd: str, silent=False) -> Result:
+    if not silent:
+        print(f"Running: {cmd}")
+
+    hide = True if silent else None
+
+    result = ctx.run(cmd, pty=True, hide=hide)
+    assert result and result.ok
+    return result
+
+
+@task
+def publish_client(ctx: Context):
+    """Publish RADIS Client to PyPI
+
+    - Make sure PyPI API token is set: poetry config pypi-token.pypi your-api-token
+    - Set version in radis_client/pyproject.toml
+    - Execute with `invoke publish-client`
+    """
+    with ctx.cd("radis_client"):
+        run_cmd(ctx, "poetry publish --build")
