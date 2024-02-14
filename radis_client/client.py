561dbe6ab9ca9998d14ce9661d483962f9ae8d26
Kai Schlamp <kai.schlamp@gmail.com>
2024-02-14 16:10:48 +0000

    Type hint report data

--- radis_client/client.py
+++ radis_client/client.py
@@ -1,4 +1,26 @@
+from datetime import date, datetime
+from typing import Literal, TypedDict
+
 import requests
+
+
+class ReportData(TypedDict):
+    document_id: str
+    groups: list[int]
+    pacs_aet: str
+    pacs_name: str
+    patient_id: str
+    patient_birth_date: date
+    patient_sex: Literal["F", "M", "U"]
+    study_instance_uid: str
+    accession_number: str
+    study_description: str
+    study_datetime: datetime
+    series_instance_uid: str
+    modalities_in_study: list[str]
+    sop_instance_uid: str
+    references: list[str]
+    body: str
 
 
 class RadisClient:
@@ -9,14 +31,16 @@ class RadisClient:
         self._reports_url = f"{self.server_url}/api/reports/"
         self._headers = {"Authorization": f"Token {self.auth_token}"}
 
-    def add_report(self, data: dict) -> requests.Response:
+    def add_report(self, data: ReportData) -> requests.Response:
         response = requests.post(self._reports_url, json=data, headers=self._headers)
         response.raise_for_status()
         return response
 
-    def retrieve_report(self, document_id: str) -> requests.Response:
+    def retrieve_report(self, document_id: str, full: bool = False) -> requests.Response:
         response = requests.get(
-            f"{self._reports_url}{document_id}/", headers=self._headers
+            f"{self._reports_url}{document_id}/",
+            headers=self._headers,
+            params={"full": full},
         )
         response.raise_for_status()
         return response
@@ -29,8 +53,6 @@ class RadisClient:
         return response
 
     def delete_report(self, document_id: str) -> requests.Response:
-        response = requests.delete(
-            f"{self._reports_url}{document_id}/", headers=self._headers
-        )
+        response = requests.delete(f"{self._reports_url}{document_id}/", headers=self._headers)
         response.raise_for_status()
         return response
