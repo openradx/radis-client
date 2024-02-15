62513ead25af6702b1a1c8b7e093bfef0699540b
Kai Schlamp <kai.schlamp@gmail.com>
2024-02-15 08:23:10 +0000

    Convert data and datetime to iso string before sending

--- radis_client/client.py
+++ radis_client/client.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime
-from typing import Literal, TypedDict
+from typing import Any, Literal, TypedDict
 
 import requests
 
@@ -23,6 +23,15 @@ class ReportData(TypedDict):
     body: str
 
 
+def _sanitize_data(data: ReportData) -> dict[str, Any]:
+    sanitized_data = {**data}
+    if isinstance(sanitized_data["patient_birth_date"], date):
+        sanitized_data["patient_birth_date"] = sanitized_data["patient_birth_date"].isoformat()
+    if isinstance(sanitized_data["study_datetime"], datetime):
+        sanitized_data["study_datetime"] = sanitized_data["study_datetime"].isoformat()
+    return sanitized_data
+
+
 class RadisClient:
     def __init__(self, server_url: str, auth_token: str):
         self.server_url = server_url
@@ -32,7 +41,9 @@ class RadisClient:
         self._headers = {"Authorization": f"Token {self.auth_token}"}
 
     def add_report(self, data: ReportData) -> requests.Response:
-        response = requests.post(self._reports_url, json=data, headers=self._headers)
+        response = requests.post(
+            self._reports_url, json=_sanitize_data(data), headers=self._headers
+        )
         response.raise_for_status()
         return response
 
@@ -45,9 +56,9 @@ class RadisClient:
         response.raise_for_status()
         return response
 
-    def update_report(self, document_id: str, data: dict) -> requests.Response:
+    def update_report(self, document_id: str, data: ReportData) -> requests.Response:
         response = requests.put(
-            f"{self._reports_url}{document_id}/", json=data, headers=self._headers
+            f"{self._reports_url}{document_id}/", json=_sanitize_data(data), headers=self._headers
         )
         response.raise_for_status()
         return response
