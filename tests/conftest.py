80df43211482afb0d6b2842da0091ebbe0c9bfcf
Kai Schlamp <kai.schlamp@gmail.com>
2024-03-22 00:24:05 +0000

    Add live server test

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+from datetime import date, datetime
+
+import pytest
+from radis.accounts.models import User
+from radis.core.utils.auth_utils import add_user_to_group
+from radis.reports.models import Group
+from radis.token_authentication.models import Token
+
+from radis_client.client import ReportData
+
+
+@pytest.fixture
+def admin_with_group_and_token():
+    user: User = User.objects.create_superuser("admin")
+    group = Group.objects.create(name="Staff")
+    add_user_to_group(user, group)
+    _, token = Token.objects.create_token(user, "", None)
+    return user, group, token
+
+
+@pytest.fixture
+def report_data():
+    group = Group.objects.create(name="Thoraxklinik")
+
+    return ReportData(
+        document_id="synapse_3dfidii5858-6633i4-ii398841",
+        language="en",
+        groups=[group.id],
+        pacs_aet="synapse",
+        pacs_name="Synapse",
+        patient_id="1234578",
+        patient_birth_date=date(1976, 5, 23),
+        patient_sex="M",
+        study_description="CT of the Thorax",
+        study_datetime=datetime(2000, 8, 10, 11, 37),
+        modalities=["CT", "PET"],
+        links=["http://synapse.net/34343-34343-34343"],
+        metadata={
+            "study_instance_uid": "34343-34343-34343",
+            "accession_number": "345348389",
+            "series_instance_uid": "34343-676556-3343",
+            "sop_instance_uid": "35858-384834-3843",
+        },
+        body="This is the report",
+    )
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,38 +1,31 @@
-from datetime import date, datetime
-
 import pytest
+from pytest_mock import MockerFixture
 from radis.reports.api.serializers import ReportSerializer
-from radis.reports.models import Group
 
-from radis_client.client import ReportData
+from radis_client.client import RadisClient
 
 
 @pytest.mark.django_db
-def test_report_data_valid():
-    group = Group.objects.create(name="Thoraxklinik")
-
-    report_data = ReportData(
-        document_id="synapse_3dfidii5858-6633i4-ii398841",
-        language="en",
-        groups=[group.id],
-        pacs_aet="synapse",
-        pacs_name="Synapse",
-        patient_id="1234578",
-        patient_birth_date=date(1976, 5, 23),
-        patient_sex="M",
-        study_description="CT of the Thorax",
-        study_datetime=datetime(2000, 8, 10, 11, 37),
-        modalities=["CT", "PET"],
-        links=["http://synapse.net/34343-34343-34343"],
-        metadata={
-            "study_instance_uid": "34343-34343-34343",
-            "accession_number": "345348389",
-            "series_instance_uid": "34343-676556-3343",
-            "sop_instance_uid": "35858-384834-3843",
-        },
-        body="This is the report",
-    )
-
+def test_report_data_valid(report_data):
     report = ReportSerializer(data=report_data.to_dict())
 
     assert report.is_valid()
+
+
+@pytest.mark.django_db
+def test_report_data_post(
+    live_server,
+    mocker: MockerFixture,
+    admin_with_group_and_token,
+    report_data,
+):
+    # Make sure it won't try to save created reports to any full text search database
+    # as those are not available during test
+    mocker.patch("radis.reports.api.viewsets.reports_created_handlers", return_value=[])
+
+    _, _, token = admin_with_group_and_token
+
+    client = RadisClient(live_server, token)
+    response = client.create_report(report_data)
+
+    assert response
